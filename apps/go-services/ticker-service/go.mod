681e51a91ce9cd2c8fd691b7d3efc6ccf8e2ea41
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-09-24 12:23:48 +0530

    Testing go lib package functions

--- apps/go-services/ticker-service/go.mod
+++ apps/go-services/ticker-service/go.mod
@@ -1,3 +1,12 @@
 module github.com/RedHat007-rgb/day-phase-1-24th/apps/go-services/ticker-service
 
 go 1.25.0
+
+require (
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
+	google.golang.org/grpc v1.75.1 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+)
--- apps/go-services/ticker-service/main.go
+++ apps/go-services/ticker-service/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/RedHat007-rgb/day-phase-1-24th/packages/golib/ws"
+	"github.com/gorilla/websocket"
+	"google.golang.org/grpc"
+)
+func BinanceConnection(url string) *websocket.Conn{
+	 conn:=ws.ConnectToWs(url)
+	return conn
+}
+
 
 func main(){
-	fmt.Println("hello")
+	lis,err:=net.Listen("tcp",":50051")
+	if err!=nil{
+		log.Fatalln("cannot establish connection",err)
+	}
+	// webby:=BinanceConnection("wss://stream.binance.com:9443/ws/btcusdc@ticker")
+	// _,msg,err:=webby.ReadMessage()
+	// if err!=nil{
+	// 	log.Fatalln("reading error of messages",err)
+	// }
+	fmt.Println(string(msg))
+	grpcServer:=grpc.NewServer()
+	log.Println("go service running on 50051")
+	if err:=grpcServer.Serve(lis);err!=nil{
+		log.Fatalf("failed to serve %v",err)
+	}
+	
+
 }
