c8b98230bc28456ba329d25e998970ea46e04a92
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-09-25 07:08:11 +0530

    added docker-compose file

--- apps/go-services/ticker-service/main.go
+++ apps/go-services/ticker-service/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
+	"github.com/RedHat007-rgb/day-phase-1-24th/packages/golib/redis"
 	"github.com/RedHat007-rgb/day-phase-1-24th/packages/golib/ws"
+
 	"github.com/gorilla/websocket"
 	"google.golang.org/grpc"
 )
@@ -15,17 +18,33 @@ func BinanceConnection(url string) *websocket.Conn{
 }
 
 
+
+
 func main(){
+	ctx:=context.Background()
 	lis,err:=net.Listen("tcp",":50051")
 	if err!=nil{
 		log.Fatalln("cannot establish connection",err)
 	}
-	// webby:=BinanceConnection("wss://stream.binance.com:9443/ws/btcusdc@ticker")
-	// _,msg,err:=webby.ReadMessage()
-	// if err!=nil{
-	// 	log.Fatalln("reading error of messages",err)
-	// }
+	webby:=BinanceConnection("wss://stream.binance.com:9443/ws/btcusdc@ticker")
+	_,msg,err:=webby.ReadMessage()
+	if err!=nil{
+		log.Fatalln("reading error of messages",err)
+	}
 	fmt.Println(string(msg))
+
+
+
+	rdb:=redis.NewConnection()
+	rdb.PublishToRedis(ctx,"message.btcusdt.binance",msg)
+	
+	
+	 count,err:=rdb.AddSymbol(ctx,"active_symbols:btcusdt","userid1");
+	 	if err!=nil{
+			fmt.Println("error while adding.... %v",err)
+		}else{
+			log.Println("%d",count)
+		}
 	grpcServer:=grpc.NewServer()
 	log.Println("go service running on 50051")
 	if err:=grpcServer.Serve(lis);err!=nil{
